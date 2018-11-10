57ff557484a987a99fac17f34bc1b9d00724015c
Ekaterina <43779021+Yushkaaa@users.noreply.github.com>
2018-11-10 16:01:59 +0800

    Create moon_weight.py

---
+++ moon_weight.py
@@ -0,0 +1,10 @@
+mport sys
+def moon_weight():
+    print('какой ваш вес')
+    weight = int(sys.stdin.readline())
+    print ('Введите ежегодный прирост вашего веса')
+    plus = float(sys.stdin.readline())
+    for  year in range (1, 15):
+        weight = weight + plus
+        print ('Год %s, вес %s' % ( year, weight))
+
