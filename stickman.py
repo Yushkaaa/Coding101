fce0cd5bb19ee8d0e12b3a2f585ccd746b8afb6b
Ekaterina <43779021+Yushkaaa@users.noreply.github.com>
2018-11-22 18:07:59 +0800

    add man. demo version

--- stickman.py
+++ stickman.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import random
 import time
+
 class Game:
     def __init__(self):
         self.tk = Tk()
@@ -113,8 +114,150 @@ class PlatformSprite(Sprite):
         self.image = game.canvas.create_image(x, y, \
                                               image=self.photo_image, anchor='nw')
         self.coordinates = Coords(x, y, x + width, y + height)
+#человечек
+class StickFigureSprite(Sprite):
+    def __init__(self, game):
+        Sprite.__init__(self, game)
+        self.images_left = [
+            PhotoImage(file="figure-L1.gif"),
+            PhotoImage(file="figure-L2.gif"),
+            PhotoImage(file="figure-L3.gif")
+        ]
+        self.images_right = [
+            PhotoImage(file="figure-R1.gif"),
+            PhotoImage(file="figure-R2.gif"),
+            PhotoImage(file="figure-R3.gif")
+        ]
+        #выводим на экран первое изображение
+        self.image = game.canvas.create_image(200, 470, \
+                                             image=self.images_left[0], anchor='nw')
+#x и y предназначены для хранения величин, на которые мы будем смещать фигурку
+        self.x = -2
+        self.y = 0
+#индекс текущего изображения
+        self.current_image = 0
+        self.current_image_add = 1
+#счетчик для прыжков 
+        elf.jump_count = 0
+        self.last_time = time.time()
+        self.coordinates = Coords()
+#привязка к клавищам
+        game.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        game.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        game.canvas.bind_all('<space>', self.jump)
 
-    
+    def turn_left(self, evt):
+        if self.y == 0:
+            self.x = -2
+    def turn_right(self, evt):
+        if self.y == 0:
+            self.x = 2
+    def jump(self, evt):
+        if self.y == 0:
+            self.y = -4
+            self.jump_count = 0
+
+    def animate(self):
+        if self.x != 0 and self.y == 0:
+            if time.time() - self.last_time > 0.1:
+                self.last_time = time.time()
+                self.current_image += self.current_image_add
+                if self.current_image >= 2:
+                    self.current_image_add = -1
+                if self.current_image <= 0:
+                    self.current_image_add = 1
+#часть предназначена для смены кадра в соответствии с вычисленным ранее индексом
+            if self.x < 0:
+                if self.y != 0:
+                    self.game.canvas.itemconfig(self.image, \
+                                                image=self.images_left[2])
+                else:
+                    self.game.canvas.itemconfig(self.image, \
+                                                image=self.images_left[self.current_image])
+            elif self.x > 0:
+                if self.y != 0:
+                    self.game.canvas.itemconfig(self.image, \
+                                                image=self.images_right[2])
+                else:
+                    self.game.canvas.itemconfig(self.image, \
+                                                image=self.images_right[self.current_image])
+
+#Получение позиции фигурки
+    def coords(self):
+        xy = self.game.canvas.coords(self.image)
+        self.coordinates.x1 = xy[0]
+        self.coordinates.y1 = xy[1]
+        self.coordinates.x2 = xy[0] + 27
+        self.coordinates.y2 = xy[1] + 30
+        return self.coordinates
+#Перемещение фигурки по экрану
+    def move(self):
+        self.animate()
+        if self.y < 0:
+            self.jump_count += 1
+            if self.jump_count > 20:
+                self.y = 4
+        if self.y > 0:
+            self.jump_count -= 1
+        co = self.coords()
+        left = True
+        right = True
+        top = True
+        bottom = True
+        falling = True
+        if self.y > 0 and co.y2 >= self.game.canvas_height:
+            self.y = 0
+            bottom = False
+        elif self.y < 0 and co.y1 <= 0:
+            self.y = 0
+            top = False
+        if self.x > 0 and co.x2 >= self.game.canvas_width:
+            self.x = 0
+            right = False
+            elif self.x < 0 and co.x1 <= 0:
+                self.x = 0
+                left = False
+            if self.x > 0 and co.x2 >= self.game.canvas_width:
+                self.x = 0
+                right = False
+            elif self.x < 0 and co.x1 <= 0:
+                self.x = 0
+                left = False
+#Столкновение с другими спрайтами
+            for sprite in self.game.sprites:
+                if sprite == self:
+                    continue
+                sprite_co = sprite.coords()
+                if top and self.y < 0 and collided_top(co, sprite_co):
+                    self.y = -self.y
+                    top = False
+#Столкновение нижней стороной
+                if bottom and self.y > 0 and collided_bottom(self.y, \
+                                                             co, sprite_co):
+                    self.y = sprite_co.y1 - co.y2
+                    if self.y < 0:
+                        self.y = 0
+                    bottom = False
+                    top = False
+#обработкf ситуации, когда фигурка находится на платформе и может выбежать за ее край
+                if bottom and falling and self.y == 0 \
+                   and co.y2 < self.game.canvas_height \
+                   and collided_bottom(1, co, sprite_co):
+                    falling = False
+#Столкновение слева и справа
+                if left and self.x < 0 and collided_left(co, sprite_co):
+                    self.x = 0
+                    left = False
+                if right and self.x > 0 and collided_right(co, sprite_co):
+                    self.x = 0
+                    right = False
+                if falling and bottom and self.y == 0 \
+                   and co.y2 < self.game.canvas_height:
+                    self.y = 4
+                    self.game.canvas.move(self.image, self.x, self.y)    
+
+
+        
 g = Game()
 platform1 = PlatformSprite(g, PhotoImage(file="platform1.gif"), \
                            0, 480, 100, 10)
@@ -146,5 +289,6 @@ g.sprites.append(platform7)
 g.sprites.append(platform8)
 g.sprites.append(platform9)
 g.sprites.append(platform10)
+sf = StickFigureSprite(g)
+g.sprites.append(sf)
 g.mainloop()
-
