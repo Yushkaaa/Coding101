d598ce296fbd751614e127f5746eabdb0f2bc6a8
yushkaaa <yushkaaa@iclloud.com>
2019-01-03 23:16:25 +0800

    Added Css

---
+++ Python/Codecademy/Password generator.py
@@ -0,0 +1,5 @@
+def password_generator(first_name, last_name):
+  temp_password = first_name[len(first_name)-3:] + last_name[len(last_name)-3:]
+  return temp_password
+
+temp_password = password_generator(first_name, last_name)
