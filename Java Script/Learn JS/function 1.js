1018037a0ecc700d6c3c0ba4438ea9655722f78e
Ekaterina <yushkaaa@icloud.com>
2019-07-02 20:43:12 +0800

    added new  tasks

---
+++ Java Script/Learn JS/function 1.js
@@ -0,0 +1,23 @@
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('Родители разрешили?');
+  }
+}
+/*Перепишите функцию, чтобы она 
+делала то же самое,но без if, в 
+одну строку.
+Используя оператор '?'
+Используя оператор || */
+
+function checkAge(age) {
+    return (age > 18) ? true : confirm('Родители разрешили?');
+  }
+
+function checkAge(age) {
+    return (age > 18) || confirm('Родители разрешили?');
+  }
+
+//синтаксис "?"
+//условие ? значение1 : значение2
