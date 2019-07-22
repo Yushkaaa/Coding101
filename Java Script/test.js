96bc925009c56ba27790dff2905b033cf02a7b82
Ekaterina <yushkaaa@icloud.com>
2019-07-22 15:33:06 +0800

    new file

---
+++ Java Script/test.js
@@ -0,0 +1,12 @@
+// let arr1 = [1,2,3,4,5,6,7]
+// arr1.splice(1, 1)
+// console.log(arr1)
+// arr1.splice(1,0 , 'новый элемент')
+// console.log(arr1)
+
+
+let arr  = ["первый","второй", "третий", "следующий элимент будет заменен",  "слово для замены"]
+arr.splice(arr.length-1,arr.length-1,"новый элемент")
+console.log(arr)
+// arr.splice(3,3)
+// console.log(arr)
